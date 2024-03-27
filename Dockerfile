e491fc0d30e5eba67af2b0cd560d54016bf57b60
william89731 <willystile@gmail.com>
2024-03-27 19:13:46 +0100

    breaking changes

--- Dockerfile
+++ Dockerfile
@@ -1,10 +1,13 @@
-FROM node:18.18.2-alpine3.18 as build
+FROM node:18.20.0-alpine3.19 as build
 WORKDIR /logs
 COPY package.json index.js  /logs/
-RUN npm install
+RUN set -ex && \ 
+    npm install \
+    --unsafe-perm --no-update-notifier \ 
+    --no-audit --omit=dev
 
 FROM alpine:latest as final
 COPY --from=build /logs /logs
 WORKDIR /logs
 RUN apk add --update  npm coreutils  --no-cache
-CMD npm start  
+CMD node index.js
--- index.js
+++ index.js
@@ -1,69 +1,67 @@
-//require('events').EventEmitter.defaultMaxListeners = 20;
-require('dotenv').config();
-const mqtt = require('mqtt');
-const { exec } = require("child_process");
+require('dotenv').config()
+const mqtt = require('mqtt')
+const { exec } = require('child_process')
+const fs = require('fs')
 const host = process.env.HOST
 const port = process.env.PORT
+const hostname = process.env.HOSTNAME
 const clientId = `logs2mqtt_${Math.random().toString(8).slice(3)}`
 const connectUrl = `mqtt://${host}:${port}`
 const client = mqtt.connect(connectUrl, {
   clientId,
-  //clean: true,
-  //connectTimeout: 4000,
-  username: process.env.USERNAME,
-  password: process.env.PASSWORD,
-  reconnectPeriod: 5000,
-});
-const topic = 'logs2mqtt/ipFail';
+  // clean: true,
+  // connectTimeout: 4000,
+  username: process.env.MQTT_USERNAME,
+  password: process.env.MQTT_PASSWORD,
+  reconnectPeriod: 5000
+})
+const topic = `logs2mqtt/${hostname}`
 
 client.on('connect', () => {
   console.log('Connected to mqtt broker')
-});
+})
 
-client.on("error", function (error) {
-  console.log("Error occurred: " + error);
-  process.kill(process.pid, 'SIGTERM');
-});
+client.on('error', function (error) {
+  console.log('Error occurred: ' + error)
+  process.kill(process.pid, 'SIGTERM')
+})
 
-try {
-function Command() {
-  console.log("start check authentication failure")
-  let TIME = process.env.TIME;
-  exec(`awk -v d1="$(date --date="${TIME}" "+%b %_d %H:%M")" \
+fs.watchFile(
+  '/var/log/auth.log',
+  {
+    bigint: false,
+    persistent: true,
+    interval: 4000
+  },
+  (log) => {
+    // value = fs.readFileSync('/var/log/auth.log', 'utf8')
+    if (log) {
+      console.log('start check authentication failure')
+      exec(`awk -v d1="$(date --date="-1 min" "+%b %_d %H:%M")" \
             -v d2="$(date "+%b %_d %H:%M")" '$0 > d1 && $0 < d2 ||
             $0 ~ d2' /var/log/auth.log | egrep "Failed password" | awk 'END{ print $11 }' | cut  -d"=" -f 2`, (error, stdout, stderr) => {
-
-    if (error) {
-      console.log(`error: ${error.message}`);
-      return;
-    }
-
-    if (stderr) {
-      console.log(`stderr: ${stderr}`);
-      return;
-    }
-
-    if (stdout.length > 10) {
-       let Warn = ` ${stdout}`  ;
-       client.publish(topic, stdout, { qos: 0, retain: false }, (error) => {
-        console.log("found ip: " + stdout)
         if (error) {
-          console.error(error)
+          console.log(`error: ${error.message}`)
+          return
+        }
+
+        if (stderr) {
+          console.log(`stderr: ${stderr}`)
+          return
+        }
+
+        if (stdout.length > 10) {
+          const Warn = ` ${stdout}`
+
+          client.publish(topic, Warn, { qos: 0, retain: false }, (error) => {
+            console.log('found ip: ' + stdout)
+            if (error) {
+              console.error(error)
+            }
+          })
+        } else {
+          console.log('no ip found')
         }
       })
-    } else {
-      console.log("no ip found")
     }
   })
- }
- setTimeout(Command, 10 * 1000);
-} catch (e) {
-  console.error(e);
-};
-
-function Delay() {
-  console.log("done!")
-  process.exit();
-}
-setTimeout(Delay, 20 * 1000);
-
